61c56e40f1f65c08cba193cfeed9d2776a115e34
pavlo <nadazasada@gmail.com>
2026-07-03 16:25:56 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,94 @@
+"use strict";
+// First Promise
+const firstPromise = new Promise((resolve, reject)=>{
+    const timer = setTimeout(()=>{
+        reject(new Error("First promise was rejected"));
+    }, 3000);
+    const onClick = (evet)=>{
+        if (event.button === 0) {
+            clearTimeout(timer);
+            document.removeEventListener("click", onClick);
+            resolve("First promise was resolved");
+        }
+    };
+    document.addEventListener("click", onClick);
+});
+firstPromise.then(()=>{
+    const div = document.createElement("div");
+    div.dataset.qa = "notification";
+    div.className = "success";
+    div.textContent = "First promise was resolved";
+    document.body.append(div);
+}, (_error)=>{
+    const div = document.createElement("div");
+    div.dataset.qa = "notification";
+    div.className = "error";
+    div.textContent = "First promise was rejected";
+    document.body.append(div);
+});
+firstPromise.catch((error)=>{
+    const div = document.createElement("div");
+    div.dataset.qa = "notification";
+    div.className = "error";
+    div.textContent = "First promise was rejected";
+    document.body.append(div);
+    return error;
+});
+// ------------------------------------------------
+// Second Promise
+const secondPromise = new Promise((resolve)=>{
+    const onLeftClick = ()=>{
+        document.removeEventListener("click", onLeftClick);
+        document.removeEventListener("contextmenu", onRightClick);
+        resolve("Second promise was resolved");
+    };
+    const onRightClick = (evet)=>{
+        event.preventDefault();
+        document.removeEventListener("click", onLeftClick);
+        document.removeEventListener("contextmenu", onRightClick);
+        resolve("Second promise was resolved");
+    };
+    document.addEventListener("click", onLeftClick);
+    document.addEventListener("contextmenu", onRightClick);
+});
+secondPromise.then(()=>{
+    const div = document.createElement("div");
+    div.dataset.qa = "notification";
+    div.className = "success";
+    div.textContent = "Second promise was resolved";
+    document.body.append(div);
+});
+// ------------------------------------------------
+// Third Promise
+const thirdPromise = new Promise((resolve)=>{
+    let leftClicked = false;
+    let rightClicked = false;
+    const onLeftClick = ()=>{
+        leftClicked = true;
+        if (leftClicked && rightClicked) {
+            document.removeEventListener("click", onLeftClick);
+            document.removeEventListener("contextmenu", onRightClick);
+            resolve("Third promise was resolved");
+        }
+    };
+    const onRightClick = (evet)=>{
+        event.preventDefault();
+        rightClicked = true;
+        if (leftClicked && rightClicked) {
+            document.removeEventListener("click", onLeftClick);
+            document.removeEventListener("contextmenu", onRightClick);
+            resolve("Third promise was resolved");
+        }
+    };
+    document.addEventListener("click", onLeftClick);
+    document.addEventListener("contextmenu", onRightClick);
+});
+thirdPromise.then(()=>{
+    const div = document.createElement("div");
+    div.dataset.qa = "notification";
+    div.className = "success";
+    div.textContent = "Third promise was resolved";
+    document.body.append(div);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
